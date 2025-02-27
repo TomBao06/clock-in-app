e96dbba7284ac3844b7cb9eda72067c43aad3555
TomBao06 <112390244+TomBao06@users.noreply.github.com>
2025-02-27 14:03:49 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function updateTime() {
+    // new Date().toLocaleTimeString() → Gets the current time. 
+    //document.getElementById("currentTime").innerText = ... → Updates the time display every second.
+    document.getEleme
